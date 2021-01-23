8b553a22f545100bd5a85ca8517193a1d034e0d8
PNU-Seung <38664481+PNU-Seung@users.noreply.github.com>
2021-01-24 00:37:06 +0900

    Update main.dart

--- calendar/plato_calendar/lib/main.dart
+++ calendar/plato_calendar/lib/main.dart
@@ -53,6 +53,31 @@ class DataSource extends CalendarDataSource {
   DataSource(List<Appointment> source) {
     appointments = source;
   }
+  
+  @override
+  DateTime getStartTime(int index) {
+    return appointments[index].from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return appointments[index].to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return appointments[index].eventName;
+  }
+
+  @override
+  Color getColor(int index) {
+    return appointments[index].background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments[index].isAllDay;
+  }
 }
 
 DataSource _getCalendarDataSource() {
