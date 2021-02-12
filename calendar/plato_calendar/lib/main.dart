451a507c6d8520b9918eea7852547145dda0b624
seung <38664481+seunggil1@users.noreply.github.com>
2021-02-12 15:26:46 +0900

    plato login

--- calendar/plato_calendar/lib/main.dart
+++ calendar/plato_calendar/lib/main.dart
@@ -1,9 +1,21 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:plato_calendar/ics.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 import './plato.dart';
+
+// 프록시 사용할 떄 주석 해제 처리.
+// class MyHttpOverrides extends HttpOverrides{
+//   @override
+//   HttpClient createHttpClient(SecurityContext context){
+//     return super.createHttpClient(context)
+//       ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+//   }
+// }
 void main() {
+  // HttpOverrides.global = new MyHttpOverrides();
   runApp(MyApp());
 }
 
--- calendar/plato_calendar/lib/plato.dart
+++ calendar/plato_calendar/lib/plato.dart
@@ -1,52 +1,61 @@
-import 'dart:convert';
-import 'dart:io';
-import 'package:http/http.dart';
-
+import 'package:dio/dio.dart';
 
 class Plato {
-  Client client = Client();
   String id ="";
   String pw = "";
-
-  String sesskey = "";
   String moodleSession = "";
 
   Future<bool> login() async {
 
-    String body = 'username=$id&password=${Uri.encodeQueryComponent(pw)}';
-    Response response = await post("https://plato.pusan.ac.kr/login/index.php",
-      headers: {
-        'Host': 'plato.pusan.ac.kr',
-        'Connection' : 'keep-alive',
-        'Content-Length': body.length.toString(),
-        'Pragma': 'no-cache',
-        'Cache-Control': 'no-cache',
-        'sec-ch-ua': '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
-        'sec-ch-ua-mobile': '?0',
-        'Upgrade-Insecure-Requests': '1',
-        'Origin': 'https://plato.pusan.ac.kr',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'Sec-Fetch-Site' : 'same-origin',
-        'Sec-Fetch-Mode' : 'navigate',
-        'Sec-Fetch-User' : '?1',
-        'Sec-Fetch-Dest' : 'document',
-        'Referer' : 'https://plato.pusan.ac.kr/',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7'
-      },
-      body: body);
-
-    String result = response.body;
+    String body = 'username=$id&password=${Uri.encodeQueryComponent(pw)}&loginbutton=%EB%A1%9C%EA%B7%B8%EC%9D%B8';
+    Response response;
+    
+    try{
+      response = await Dio().post("https://plato.pusan.ac.kr/login/index.php",
+      options: Options(
+        followRedirects : false,
+        contentType: "application/x-www-form-urlencoded",
+        headers: {
+          "Host": "plato.pusan.ac.kr",
+          "Connection": "close",
+          "Content-Length": body.length.toString(),
+          "Cache-Control": "max-age=0",
+          "sec-ch-ua": 'Chromium;v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
+          "sec-ch-ua-mobile": "?0",
+          "Upgrade-Insecure-Requests": "1",
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
+          "Origin" : "https://plato.pusan.ac.kr",
+          "Content-Type" : "application/x-www-form-urlencoded",
+          "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+          "Sec-Fetch-Site" : "same-origin",
+          "Sec-Fetch-Mode" : "navigate",
+          "Sec-Fetch-User" : "?1",
+          "Sec-Fetch-Dest" : "document",
+          "Referer" : "https://plato.pusan.ac.kr/",
+          "Accept-Encoding" : "gzip, deflate",
+          "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+        }
+      ),
+      data: body);
+    }
+    catch(e){
+      if(e.runtimeType == DioError && e.error == "Http status error [303]")
+        response = e.response;
+      else{
+        print("plato Login Error: ${e.error}");
+        return false;
+      }
+    }
+    moodleSession = response.headers.map["set-cookie"][1];
+    moodleSession = moodleSession.substring(0, moodleSession.indexOf(';'));
     return true;
   }
+  
+  Future<bool> getCalendar() async{
 
-
+  }
   Future<bool> logout() async{
-
-    client.close();
-
+    print(1);
     return true;
   }
 
